eb96937e7992a74d4fc73a55419de0d21bed89cc
unknown <darajsv@gmail.com>
2021-10-04 14:31:25 -0300

    title page adjustments

--- src/components/Planets/individualPlanet/IndividualPlanet.jsx
+++ src/components/Planets/individualPlanet/IndividualPlanet.jsx
@@ -1,38 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import api from '../../../services/api.js'
-import './individualPlanet.css'
+import api from "../../../services/api.js";
+import "./individualPlanet.css";
 
 const IndividualPlanet = () => {
-    const params = useParams();
-    const [data, setData] = useState([]);
-    
-    useEffect(() => {
-        api.get(`/planets/${params.planetId}/`).then((response) => {
-            setData(response.data);
-        }).catch((err) => {
-            console.error(err);
-        })
-    }, [params.planetId, setData])
+  const params = useParams();
+  const [data, setData] = useState([]);
 
-        return(
-            <div className='individualPlanet'>
-               <div className='planetName'>
-                   <h1>{data.name}</h1>
-               </div>
-               <div className='informationCard'>
-                   <h5> Rotation Period: {data.rotation_period} </h5>
-                   <h5> Oribital Period: {data.orbital_period} </h5>
-                   <h5> Diameter: {data.diameter} </h5>
-                   <h5> Climate: {data.climate} </h5>
-                   <h5> Gravity: {data.gravity} </h5>
-                   <h5> Terrain: {data.terrain} </h5>
-                   <h5> Surface Water: {data.surface_water} </h5>
-                   <h5> Population: {data.population}</h5>
-               </div>
-            </div>
-        )
-    
-}
+  useEffect(() => {
+    api
+      .get(`/planets/${params.planetId}/`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [params.planetId, setData]);
+
+  return (
+    <div className="individualPlanet">
+      <div className="planetName">
+        <h1>{data.name}</h1>
+      </div>
+      <div className="informationCard">
+        <h5> Rotation Period: {data.rotation_period} </h5>
+        <h5> Oribital Period: {data.orbital_period} </h5>
+        <h5> Diameter: {data.diameter} </h5>
+        <h5> Climate: {data.climate} </h5>
+        <h5> Gravity: {data.gravity} </h5>
+        <h5> Terrain: {data.terrain} </h5>
+        <h5> Surface Water: {data.surface_water} </h5>
+        <h5> Population: {data.population}</h5>
+      </div>
+    </div>
+  );
+};
 
 export default IndividualPlanet;
--- src/components/TitleScreen/Title.jsx
+++ src/components/TitleScreen/Title.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./title.css";
 import Card from "./TitlePageCard/Card.jsx";
 import images from "../../assets/img/titleScreenImg/deathStar.jpg";
+import { Link } from "react-router-dom";
 
 const Title = () => {
   return (
@@ -9,17 +10,29 @@ const Title = () => {
       <div>
         <h1>SWWIKI</h1>
         <h3>A Star Wars Wiki</h3>
-        <h5>
+        <h5 className='titleDescription'>
           integrated with <a href="https://swapi-trybe.herokuapp.com/">swapi</a>
         </h5>
       </div>
       <div className="cardSection">
-        <Card img={images} alt="my image" title="Planets" />
-        <Card img={images} alt="my image" title="People" />
-        <Card img={images} alt="my image" title="Species" />
-        <Card img={images} alt="my image" title="Films" />
-        <Card img={images} alt="my image" title="Vehicles" />
-        <Card img={images} alt="my image" title="Starships" />
+        <Link to="/planets">
+          <Card img={images} alt="my image" title="Planets" />
+        </Link>
+        <Link to="/people">
+          <Card img={images} alt="my image" title="People" />
+        </Link>
+        <Link to="/species">
+          <Card img={images} alt="my image" title="Species" />
+        </Link>
+        <Link to="/films">
+          <Card img={images} alt="my image" title="Films" />
+        </Link>
+        <Link to="/vehicles">
+          <Card img={images} alt="my image" title="Vehicles" />
+        </Link>
+        <Link to="/starships">
+          <Card img={images} alt="my image" title="Starships" />
+        </Link>
       </div>
     </div>
   );
